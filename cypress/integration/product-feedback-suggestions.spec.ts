004455e8edfc575313e5516caf81affaec67cbd5
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-26 14:33:35 +0200

    Add fixtures and stub network calls

--- cypress/integration/product-feedback-suggestions.spec.ts
+++ cypress/integration/product-feedback-suggestions.spec.ts
@@ -1,18 +1,25 @@
 describe('Product feedback suggestion page', () => {
   beforeEach(() => {
+    // Stubbing network call
+    cy.intercept('GET', '/product-feedbacks', {
+      fixture: 'product-feedbacks.json',
+    }).as('getProductFeedbacks')
+
     // visit the suggestions page
     cy.visit('/')
   })
 
   it.only('User should be able to see the product feedback suggestions list', () => {
-    cy.visit('/')
+    // pass a Route Alias that forces Cypress to wait
+    // until it sees a response for the request that matches this alias
+    cy.wait(['@getProductFeedbacks'])
   })
 
   it('User should be able to only see product feedbacks with "suggestion" status', () => {
-    cy.visit('/')
+    cy.wait(['@getProductFeedbacks'])
   })
 
   it('User should be able to see the product feedback list ordererd by upvote score', () => {
-    cy.visit('/')
+    cy.wait(['@getProductFeedbacks'])
   })
 })
