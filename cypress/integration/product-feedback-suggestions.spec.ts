d7b52800a1921b57c95f473c81cdf98e8f14e77e
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-26 16:12:02 +0200

    Specify and pass 'User should be able to only see product feedbacks with suggestion status'

--- cypress/integration/product-feedback-suggestions.spec.ts
+++ cypress/integration/product-feedback-suggestions.spec.ts
@@ -9,7 +9,7 @@ describe('Product feedback suggestion page', () => {
     cy.visit('/')
   })
 
-  it.only('User should be able to see the product feedback suggestions list', () => {
+  it('User should be able to see the product feedback suggestions list', () => {
     // pass a Route Alias that forces Cypress to wait
     // until it sees a response for the request that matches this alias
     cy.wait(['@getProductFeedbacks'])
@@ -31,10 +31,19 @@ describe('Product feedback suggestion page', () => {
   })
 
   it('User should be able to only see product feedbacks with "suggestion" status', () => {
-    cy.wait(['@getProductFeedbacks'])
-  })
+    const mockProductFeedbackWithSuggestionStatusCount = 6
 
-  it('User should be able to see the product feedback list ordererd by upvote score', () => {
     cy.wait(['@getProductFeedbacks'])
+
+    // Expects suggestions list is composed only with product feedback
+    // with "suggestion" status
+    cy.findByRole('list', {
+      name: /suggestions/i,
+    }).within(() => {
+      // Find by aria "listitem" role to query all the listitem under the list
+      cy.findAllByRole('listitem')
+        .its('length')
+        .should('eq', mockProductFeedbackWithSuggestionStatusCount)
+    })
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-type SuggestionType = {
+type ProductFeedbackType = {
   description: string
   id: number
+  status: string
+}
+
+type RequestProductFeedbackType = {
+  data: ProductFeedbackType[]
 }
 
 function App() {
-  const [suggestions, setSuggestions] = useState<SuggestionType[]>([])
+  const [suggestions, setSuggestions] = useState<ProductFeedbackType[]>([])
 
   useEffect(() => {
-    axios.get('/product-feedbacks').then((res: any) => {
-      setSuggestions(res.data.data)
+    axios.get<RequestProductFeedbackType>('/product-feedbacks').then((res) => {
+      const productFeedbackFiltredBySuggestionStatus = res.data.data.filter(
+        (suggestion: ProductFeedbackType) => {
+          return suggestion.status === 'suggestion'
+        }
+      )
+      setSuggestions(productFeedbackFiltredBySuggestionStatus)
     })
   }, [])
 
@@ -19,7 +29,7 @@ function App() {
     <section>
       <h1 id='suggestions-heading'>Suggestions</h1>
       <ul aria-labelledby='suggestions-heading'>
-        {suggestions.map((suggestion: SuggestionType) => (
+        {suggestions.map((suggestion: ProductFeedbackType) => (
           <li key={suggestion.id}>{suggestion.description}</li>
         ))}
       </ul>
