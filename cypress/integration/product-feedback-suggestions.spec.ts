3e888c3335da09870b58cd4bfcc35cee4687f671
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-26 14:31:47 +0200

    Convert user stories and functional specs into partial test specs

---
+++ cypress/integration/product-feedback-suggestions.spec.ts
@@ -0,0 +1,18 @@
+describe('Product feedback suggestion page', () => {
+  beforeEach(() => {
+    // visit the suggestions page
+    cy.visit('/')
+  })
+
+  it.only('User should be able to see the product feedback suggestions list', () => {
+    cy.visit('/')
+  })
+
+  it('User should be able to only see product feedbacks with "suggestion" status', () => {
+    cy.visit('/')
+  })
+
+  it('User should be able to see the product feedback list ordererd by upvote score', () => {
+    cy.visit('/')
+  })
+})
