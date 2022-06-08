b6967516e378384e53affd3ae2c2078a1c096eb1
Youness BENNAJ <youness.bennaj@gmail.com>
2022-06-08 11:42:10 +0200

    specify and pass select field visual tests

--- src/components/atoms/SelectField.tsx
+++ src/components/atoms/SelectField.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import iconDown from '../../assets/icon-arrow-down.svg'
+
 const SelectFieldStyled = styled.select`
-  background: #f7f8fd;
   border: none;
   border-radius: 5px;
   padding: 12px 24px;
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background: transparent;
+  background-image: url(${iconDown});
+  background-repeat: no-repeat;
+  background-position-x: calc(100% - 24px);
+  background-position-y: 50%;
+  background-color: #f7f8fd;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+  color: #3a4374;
+  width: 100%;
+
+  &:focus {
+    border: none;
+    border: 1px solid #4661e6;
+  }
 `
 
 export const SelectField: React.FC = () => {
-  return <SelectFieldStyled></SelectFieldStyled>
+  return (
+    <SelectFieldStyled id='category' name='category' value={'feature'}>
+      <option value='feature'>Feature</option>
+      <option value='ui'>UI</option>
+      <option value='ux'>UX</option>
+      <option value='enhancement'>Enhancement</option>
+      <option value='bug'>Bug</option>
+    </SelectFieldStyled>
+  )
 }
--- src/stories/atoms/SelectField.stories.tsx
+++ src/stories/atoms/SelectField.stories.tsx
@@ -15,3 +15,13 @@ DefaultSelectField.parameters = {
     url: 'https://www.figma.com/file/csaK8VSg1ojbjr3lrqvSz3/product-feedback-app?node-id=0%3A2602',
   },
 }
+
+export const SelectFieldWithActiveState = () => <SelectField />
+
+SelectFieldWithActiveState.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/csaK8VSg1ojbjr3lrqvSz3/product-feedback-app?node-id=729%3A2',
+  },
+  pseudo: { focus: true },
+}
